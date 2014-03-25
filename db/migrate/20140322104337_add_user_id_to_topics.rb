a503d00a2fd8d92c95a1b3ac8a09d7791dece2af
Charlie <annecharlotte.gerard@gmail.com>
2014-03-25 14:36:54 +1100

    changes

--- db/migrate/20140322104337_add_user_id_to_topics.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToTopics < ActiveRecord::Migration
-  def change
-  	add_column :topics, :user_id, :integer
-  end
-end
--- db/migrate/20140324050735_add_user_id_to_sources.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToSources < ActiveRecord::Migration
-  def change
-  	add_column :sources, :user_id, :integer
-  end
-end
