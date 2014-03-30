581414299bb65f1dbda14ac88a25f8b1ee66450c
Mathilda Thompson <mathildathompson288@gmail.com>
2014-03-30 20:46:05 +1100

    Revisions

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -3,27 +3,30 @@ class SourcesController < ApplicationController
     @source = Source.all
   end
 
+  #Keep your routes in order of the flow of the website; 
+  def new
+    @new_source = Source.new
+    @topic = Topic.all
+  end
+
   def create
-    @source = Source.all
-    @own_source = Source.create params[:source]
-    @own_source.topic_id = params[:name][:id]
-    #adds the source to the user's sources if an rss feed can be found
-    input_url = @own_source.url
-    @rss = Feedbag.find(input_url).first
-    #Add own sources to user sources if it already doesn't have it.
-    if @rss && @current_user.sources.select{|source| source.url == @rss } == []
-      @own_source.save
-      @current_user.sources << @own_source
-      redirect_to root_path
+    #Before you create a source object, check if the source has a rss feed;
+    @rss = Feedbag.find(params['source']['url']).first
+    if @rss 
+      @own_source = Source.create params[:source]
+      @own_source.topic_id = params[:name][:id]
+      if @own_source.save #If the source was saved then it cannot have already been in the database, this means the user can not have already had it as a source; 
+        #adds the source to the user's sources if an rss feed can be found
+        @current_user.sources << @own_source
+        redirect_to root_path
+      else
+        flash[:notice] = "This source is already in the database and you can follow it"
+        redirect_to new_source_path
+      end
     else
       flash[:notice] = "Oops this url cannot be added, please try with another one"
       redirect_to new_source_path
     end
-  end
-
-  def new
-    @new_source = Source.new
-    @topic = Topic.all
   end
 
   def edit
@@ -31,29 +34,20 @@ class SourcesController < ApplicationController
 
   def show
     @source = Source.find params[:id]
-    sloppy_url = @source.url
-    rss = Feedbag.find(sloppy_url).first
-    feeds = Feedjira::Feed.fetch_and_parse(rss)
-    @response = HTTParty.get (rss)
+    @response = @source.get_rss
+    #It is best practice to keep your controllers thin and put logic into methods in the model; 
   end
 
   def follow
+    #I do not think that we need to check the soure for rss here, we did that when we created the source; 
     source = Source.find params[:id]
-    sloppy_url = source.url
-    #Look for rss feed from the url and if found, parse the content.
-    rss = Feedbag.find(sloppy_url).first
-    if rss.present?
-      feeds = Feedjira::Feed.fetch_and_parse(rss)
-      @response = HTTParty.get (rss)
-    end
-
-    topic = Topic.find(source.topic_id)
-    #the sources are not added to the user's sources if the rss url cannot be found.
-    if @response["rss"].present?
+    if source
       @current_user.sources << source
-      topic.users << @current_user
+      redirect_to root_path
+    else
+      #Add a flash message in here saying that there has been an error..
+      redirect_to :back
     end
-    redirect_to root_path
   end
 
   def unfollow
